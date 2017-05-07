c1fdf57e6e8bf94205b3087768c36daa17e84891
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-05-07 14:39:30 +0300

    Update release configuration.

--- src/test/java/com/thecoffeine/config/ConfigApplicationTest.java
+++ src/test/java/com/thecoffeine/config/ConfigApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,6 +31,7 @@ public class ConfigApplicationTest {
     public void testAvailability() throws Exception {
         this.mockMvc.perform(
             get( "/info" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 )
         )
             .andExpect( status().isOk() );
     }
